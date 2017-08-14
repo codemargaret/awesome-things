cfeaa237298146712c2d2e38024967376f1dc3c3
Margaret Berry and Nate McGregor <margaret@>
2017-08-14 09:43:02 -0700

    add js to collect input, output using arrays

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $("#awesome").submit(function(event){
+    event.preventDefault();
+    var foodInput = $("input#food").val();
+    var animalInput = $("input#animal").val();
+    var colorInput = $("input#color").val();
+    var bookInput = $("input#book").val();
+
+    var arrayOne = [foodInput, animalInput, colorInput, bookInput];
+    var arrayTwo = [];
+    arrayTwo.push(arrayOne[1],arrayOne[0],arrayOne[2]);
+    $("#things").show();
+    $("#item1").text(arrayTwo[0]);
+    $("#item2").text(arrayTwo[1]);
+    $("#item3").text(arrayTwo[2]);
+  });
+});
