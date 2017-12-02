d70fdac65d8f56fe10aa7d1d13a1d856b28a9fe5
Margaret Berry <codeberry1@gmail.com>
2017-12-02 13:26:09 -0800

    capture and show final survey answer

--- js/scripts.js
+++ js/scripts.js
@@ -8,10 +8,11 @@ $(document).ready(function(){
 
     var arrayOne = [foodInput, animalInput, colorInput, bookInput];
     var arrayTwo = [];
-    arrayTwo.push(arrayOne[1],arrayOne[0],arrayOne[2]);
+    arrayTwo.push(arrayOne[1],arrayOne[0],arrayOne[2],arrayOne[3]);
     $("#things").show();
     $("#item1").text(arrayTwo[0]);
     $("#item2").text(arrayTwo[1]);
     $("#item3").text(arrayTwo[2]);
+    $("#item4").text(arrayTwo[3]);
   });
 });
